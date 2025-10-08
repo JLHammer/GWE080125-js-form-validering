f12d8c814fb7fac3732ff0ec34c84f8ad2bded38
Heinz K <heka@techcollege.dk>
2025-10-08 12:20:02 +0200

    Function Breakout

--- js/validate.js
+++ js/validate.js
@@ -1,29 +1,47 @@
-document.getElementById('validate').addEventListener('click', (event) => {
+document.getElementById('validate').addEventListener('click', event => {
     const form = event.target.form
     let formIsValid = true
 
-    if (!form.fullname.value) {
-        form.fullname.parentElement.classList.add('error')
-        if (!form.fullname.nextElementSibling) {
-            const errorMsg = document.createElement('span')
-            errorMsg.classList.add('error-message')
-            errorMsg.innerText = 'Du skal udfylde feltet'
-            form.fullname.parentElement.append(errorMsg)
-        }
-
+    if (!form.fullname.value) {        
         formIsValid = false
+        HandleError(form.fullname)
     }
 
-    form.fullname.addEventListener('input', () => {
-        if(form.fullname.nextElementSibling) {
-            form.fullname.nextElementSibling.remove()
-        }
-        form.fullname.parentElement.classList.remove('error')
-        formIsValid = true
-    })
+    if (!form.email.value) {        
+        formIsValid = false
+        HandleError(form.email)
+    }    
+
+    if (!form.message.value) {        
+        formIsValid = false
+        HandleError(form.message)
+    }    
 
     if (formIsValid) {
-        console.log('Alt er godt og formularen kan sendes')
+        const formData = `
+            Navn: ${form.fullname.value}\n
+            Email: ${form.email.value}\n
+            Besked: ${form.message.value}\n
+        `
+        alert(formData)
     }
 })
+
+const HandleError = element => {
+    element.parentElement.classList.add('error')
+    if (!element.nextElementSibling) {
+        const errorMsg = document.createElement('span')
+        errorMsg.classList.add('error-message')
+        errorMsg.innerText = 'Du skal udfylde feltet'
+        element.parentElement.append(errorMsg)
+    }
+
+    element.addEventListener('input', () => {
+        if (element.nextElementSibling) {
+            element.nextElementSibling.remove()
+        }
+        element.parentElement.classList.remove('error')
+        formIsValid = true
+    })
+}
 
